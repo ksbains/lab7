a1347bccae77bcfadb6825c09b2abf68022012e3
ksbains <k1bains@ucsd.edu>
2018-02-22 00:24:04 -0800

    got the like button and the viewAlt route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
+exports.viewAlt = function (request, response){
+	projects["viewAlt"] = true;
+	response.render('index', projects);
+};
 
 exports.view = function(request, response){
+	projects["viewAlt"] = false;
   	response.render('index', projects);
 };
